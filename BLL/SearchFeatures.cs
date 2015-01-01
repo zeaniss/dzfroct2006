959ad1b3df472ed1e15b9f7a906fe90775ddea07
ikbal84 <ikbal84@gmail.com>
2015-01-01 17:45:01 +0100

    Commit de Ikbal 01_01_2015

---
+++ BLL/SearchFeatures.cs
@@ -0,0 +1,22 @@
+﻿using dzfroct2006.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace dzfroct2006.BLL
+{
+    public class SearchFeatures
+    {
+        public String SearchFeatureName;
+        public String SearchFeatureValue;
+
+        public SearchFeatures()
+        {
+
+        }
+
+
+        
+    }
+}
---
+++ DAL/HotelsDBContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using dzfroct2006.Models;
+
+namespace dzfroct2006.DAL
+{
+    public class HotelsDBContext : DbContext
+    {
+        public HotelsDBContext()
+            : base("HotelsDB")
+        {
+            //Database.SetInitializer<HotelsDBContext>(new CreateDatabaseIfNotExists<HotelsDBContext>());      
+            //Database.SetInitializer<HotelsDBContext>(new DropCreateDatabaseIfModelChanges<HotelsDBContext>());
+            //Database.SetInitializer<HotelsDBContext>(new DropCreateDatabaseAlways<HotelsDBContext>());
+            Database.SetInitializer<HotelsDBContext>(new HotelsDBInitializer());
+        }
+
+        public DbSet<Hotels> Hotels { get; set; }
+        public DbSet<HotelOwners> Owner { get; set; }
+        public DbSet<HotelRooms> Rooms { get; set; }
+        public DbSet<Features> Features { get; set; }
+        public DbSet<HotelFeatures> HotelFeatures { get; set; }
+        public DbSet<HotelImages> HotelImages { get; set; }
+    }
+}
